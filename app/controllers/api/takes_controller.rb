b28dc2d715e4e35a36058ebcf106fec0b103369c
“TJ <tylerchecketts@gmail.com>
2017-12-12 19:51:16 -0700

    create model and controller

--- app/controllers/api/takes_controller.rb
+++ app/controllers/api/takes_controller.rb
@@ -1,13 +1,42 @@
 class Api::TakesController < ApplicationController
+  before_action :set_take, only: [:update, :destroy]
+  
   def index
+    render json: Take.all
   end
 
   def create
+    take = Take.new(take_params)
+    if take.save
+      render json: take
+    else
+      render json: { errors: take.errors.full_messages.join(', ') }
+    end
   end
 
   def update
+    if @take.update(take_params)
+      render json: @take
+    else
+      render json: { errors: take.errors.full_messages.join(', ') }
+    end
   end
 
   def destroy
+    @take.destroy
   end
+
+  private
+  
+    def set_take
+      @take = Take.find(params[:id])
+    end
+  
+    def take_params
+      params.require(:take).permit(
+        :author,
+        :body
+      )
+    end
+
 end
